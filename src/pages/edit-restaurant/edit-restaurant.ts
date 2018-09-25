ad6d6e15adb94e8fb62672af5562d73ed425896f
naluebet manpati <naluebet.m@gmail.com>
2018-09-26 05:49:33 +0700

    ionic-commit 6

---
+++ src/pages/edit-restaurant/edit-restaurant.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Restaurant } from '../../firebase/restaurant';
+import { Dish } from '../../firebase/dish';
+
+@IonicPage()
+@Component({
+  selector: 'page-edit-restaurant',
+  templateUrl: 'edit-restaurant.html',
+})
+export class EditRestaurantPage {
+
+  public onYourRestaurantForm: FormGroup;
+  restaurant = {} as Restaurant;
+  restaurantEdit: any;
+
+  constructor(private angularFireAuth: AngularFireAuth,
+    private angularFireDatabase: AngularFireDatabase,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private _fb: FormBuilder,
+    public loadingCtrl: LoadingController) {
+
+
+    this.restaurant = this.navParams.get('item');
+    
+    const itemIDR = this.navParams.get('item')
+    
+    this.angularFireAuth.authState.take(1).subscribe(data => {
+      this.restaurantEdit = this.angularFireDatabase.list(`restaurantID/${data.uid}/${itemIDR.key}`)
+    });
+
+    // this.angularFireAuth.authState.take(1).subscribe(data => {
+    //   // this.items = this.angularFireDatabase.list(`restaurantID/${data.uid}`).valueChanges();
+    //   this.items = this.angularFireDatabase.list(`restaurantID/${data.uid}${rtrID}`).snapshotChanges().map(caches => {
+    //     return caches.map(c => ({
+    //       key: c.payload.key, ...c.payload.val()
+    //     }));
+    //   });
+    // });
+
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EditRestaurantPage');
+  }
+
+  ngOnInit() {
+    this.onYourRestaurantForm = this._fb.group({
+      profiledata: [true, Validators.compose([
+        Validators.required
+      ])],
+      restaurantTitle: ['', Validators.compose([
+        Validators.required
+      ])],
+      restaurantAddress: ['', Validators.compose([
+        Validators.required
+      ])],
+      restaurantType: ['', Validators.compose([
+        Validators.required
+      ])]
+    });
+  }
+
+
+
+  editRestaurant(restaurant: Restaurant) {
+    const itemIDR = this.navParams.get('item')
+    const loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      spinner: 'crescent',
+    });
+    loader.present();
+    this.angularFireAuth.authState.take(1).subscribe(data => {
+      // this.angularFireDatabase.list(`restaurantID/${data.uid}/${itemIDR.key}`).update(restaurant);
+    });
+    loader.dismiss();
+  }
+}
--- src/pages/register-dish/register-dish.ts
+++ src/pages/register-dish/register-dish.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { Dish } from '../../firebase/dish';
 
 /**
  * Generated class for the RegisterDishPage page.
@@ -15,11 +19,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterDishPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  items: Observable<any[]>;
+  dish = {} as Dish;
+
+
+
+  constructor(private angularFireAuth: AngularFireAuth,
+    private angularFireDatabase: AngularFireDatabase,
+    public loadingCtrl: LoadingController,
+    public navCtrl: NavController, public navParams: NavParams) {
+
+    this.angularFireAuth.authState.take(1).subscribe(data => {
+      // this.items = this.angularFireDatabase.list(`restaurantID/${data.uid}`).valueChanges();
+      this.items = this.angularFireDatabase.list(`restaurantID/${data.uid}`).snapshotChanges().map(caches => {
+        return caches.map(c => ({
+          key: c.payload.key, ...c.payload.val()
+        }));
+      });
+    });
+  
   }
 
+  registerDish(dish: Dish,) {
+    
+    const loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      spinner: 'crescent',
+    });
+    loader.present();
+    this.angularFireAuth.authState.take(1).subscribe(data => {
+      this.angularFireDatabase.list(`restaurantID/${data.uid}/${dish.id}`).push(dish).then(()=>{
+        this.navCtrl.setRoot(RegisterDishPage);
+      });
+    });
+    loader.dismiss();
+  }
+
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterDishPage');
+    
   }
 
 }
